ec4631057387dc342a624c468c65fce96ba31a21
Greg Thompson <thompson.glowe@gmail.com>
2015-02-12 11:46:55 -0600

    namespacing with grunt options. still need tests

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ var customMedia = require('rework-custom-media');
 var inherit = require('rework-inherit');
 var inliner = require('rework-npm');
 var limits = require('rework-ie-limits');
+var namespace = require('rework-class-prefix');
 var remFallback = require('rework-rem-fallback');
 var rework = require('rework');
 var reworkFunction = require('rework-plugin-function');
@@ -36,6 +37,7 @@ function suit(options) {
   // base font-size for rem fallback
 
   var remMultiplier = options.remMultiplier || 16;
+  var ns = options.namespace || false;
 
   return function (ast, reworkObj) {
     reworkObj
@@ -71,5 +73,9 @@ function suit(options) {
       }))
       // rem fallback
       .use(remFallback(remMultiplier));
+    // Optional namespacing
+    if(ns){
+      reworkObj.use(namespace(ns.name, { not: ns.not }));
+    }
   };
 }
